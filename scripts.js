672e1256aadad72eda2cdb67da95da2a47bad66c
Kevin Hernandez <kevinelihern@gmail.com>
2023-07-13 19:43:38 -0500

    added color

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load',function(){
+    let status = this.document.getElementById("flightStatus");
+
+    let upButtom = this.document.getElementById("up");
+    let downButtom = this.document.getElementById("down");
+    let leftButtom = this.document.getElementById("left");
+    let rightButton = this.document.getElementById("right");
+
+    let takeoffButtom = this.document.getElementById("takeoff")
+
+    takeoffButtom.addEventListener("click",function(){
+        if(window.confirm("Confirm that the shuttle is ready for takeoff")) {
+        status = "Shuttle in Flight";
+        document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+        }
+    });
+
+
+
+    upButtom.addEventListener("click", function(){
+    });
+
+});
